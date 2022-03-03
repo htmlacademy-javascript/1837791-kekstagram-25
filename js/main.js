d5f9b85592673e65732438acf98861dfa9037b67
Vitaly <shmal9lk@gmail.com>
2022-03-03 12:44:40 +0500

    Правки

--- js/main.js
+++ js/main.js
@@ -1,19 +1,12 @@
 //Функция, возращающая случайное целое число (источник: https://myrusakov.ru/js-random-numbers.html):
 function getRandomInRange(min, max) {
-  if (min>max) {
-    return console.log('ОШИБКА! Минимальное числе не может быть больше максимального');
+  if (min >= max) {
+    throw ("Минимальное число не может быть больше, либо равно максимальному");
   }
-  if (min=max) {
-    return console.log('ОШИБКА! Минимальное и максимальное число не должны совпадать');
-  }
-
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-//Функция, проверяющая длину строки (https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/length):
-function checkStringLength(string,maxlength) {
-  if (string.length > maxlength) {
-    return false;
-  }
-  return true;
-}
 
+//Функция, проверяющая длину строки (https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/length):
+function checkStringLength(string, maxlength) {
+  return string.length > maxlength ? false : true;
+}
