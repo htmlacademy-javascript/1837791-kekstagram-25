209e92bfcb3a93677313e933188bd9f4b2d2216b
Vitaly <shmal9lk@gmail.com>
2022-03-12 12:54:22 +0500

    4.9. Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,3 +1,76 @@
+const COMMENTS_ID = fillArray(25);
+const COMMENTS_AVATAR = fillAvatarUrl(6);
+const COMMENTS_MESSAGE = ['Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+ 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+const COMMENTS_NAMES = ['Василий', 'Иван', 'Егор', 'Игорь', 'Тарас', 'Марк'];
+const PHOTO_ID = fillArray(25);
+const PHOTO_URL = fillUrl(25);
+const PHOTO_DESCRIPTION = ['Овощные культуры','Ягоды','Стручковые','Бобовые','Грибы'];
+const PHOTO_LIKES = fillArray(25).slice(14); //число лайков - массив от 15 до 200
+
+
+
+// Строим комментарии
+const createComments = () => {
+  return {
+    id: getRandomArrayElement(COMMENTS_ID),
+    avatar: getRandomArrayElement(COMMENTS_AVATAR),
+    message: getRandomArrayElement(COMMENTS_MESSAGE),
+    name:getRandomArrayElement(COMMENTS_NAMES),
+  }
+}
+
+// Записываем комментарии в массив
+const similarComments = Array.from({length:6}, createComments);
+
+
+// Строим описание фотографии
+const createDescription = () => {
+  return {
+    id: getRandomArrayElement(PHOTO_ID),
+    url: getRandomArrayElement(PHOTO_URL),
+    description: getRandomArrayElement(PHOTO_DESCRIPTION),
+    likes: getRandomArrayElement(PHOTO_LIKES),
+    comments: getRandomArrayElement(similarComments),
+  }
+}
+
+// Записываем описание в массив
+const similarDescription = Array.from({length:25}, createDescription);
+
+
+
+
+// !Вспомогательные функции!
+
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomInRange(0, elements.length - 1)];
+};
+
+//Функция, заполняющая массив числами по порядку до максимального
+function fillArray(max) {
+  let array = [];
+  if (max) for (let i = 1; i <= max;) array.push(i++);
+  return array;
+};
+//Функция, заполняющая массив URL адресами по порядку до максимального
+function fillUrl(max) {
+  let array = [];
+  if (max) for (let i = 1; i <= max;) array.push('photos/{{' + i++ + '}}.jpg');
+  return array;
+};
+
+//Функция, заполняющая массив URL адресами по порядку до максимального
+function fillAvatarUrl(max) {
+  let array = [];
+  if (max) for (let i = 1; i <= max;) array.push('img/avatar-' + i++ + '.svg');
+  return array;
+};
+
 //Функция, возращающая случайное целое число (источник: https://myrusakov.ru/js-random-numbers.html):
 function getRandomInRange(min, max) {
   if (min >= max) {
