1035171dbbe09259f806f932ef0b68f9f799253d
Vitaly <shmal9lk@gmail.com>
2022-03-14 21:28:37 +0500

    Правки очередные

--- js/main.js
+++ js/main.js
@@ -1,61 +1,44 @@
-const COMMENTS_ID = fillArray(25);
-const COMMENTS_AVATAR = fillArray(6);
-const COMMENTS_MESSAGE = ['Всё отлично!',
+const COMMENT_MESSAGE = ['Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-const COMMENTS_NAMES = ['Василий', 'Иван', 'Егор', 'Игорь', 'Тарас', 'Марк'];
-const PHOTO_ID = fillArray(25);
-const PHOTO_URL = fillArray(25);
+const COMMENT_NAME = ['Василий', 'Иван', 'Егор', 'Игорь', 'Тарас', 'Марк'];
+
 const PHOTO_DESCRIPTION = ['Овощные культуры','Ягоды','Стручковые','Бобовые','Грибы'];
-const PHOTO_LIKES = fillArray(25).slice(14); //число лайков - массив от 15 до 200
+const PHOTO_MAX_LIKES = 200;
+const PHOTO_MIN_LIKES = 15;
+const AVATARS_TOTAL = 6;
+const MAX_CARDS = 25;
 
-const getRandomArrayElement = (elements) => elements[getRandomInRange(0, elements.length - 1)];
+// Новый код
+const createComment = (id) => ({
+  id,
+  avatar: `img/avatar-${getRandomInRange(0, AVATARS_TOTAL)}.svg`,
+  message: COMMENT_MESSAGE[getRandomInRange(0, COMMENT_MESSAGE.length - 1)],
+  name: COMMENT_NAME[getRandomInRange(0, COMMENT_NAME.length - 1)],
+});
 
+const createPhotoBlock = (id) => ({
+  id,
+  url: `photos/${getRandomInRange(1, 25)}.jpg`,
+  description: PHOTO_DESCRIPTION[getRandomInRange(0, PHOTO_DESCRIPTION.length - 1)],
+  likes: getRandomInRange(PHOTO_MIN_LIKES, PHOTO_MAX_LIKES),
+  comments: Array.from({length: getRandomInRange(1, 2)}, (_, index) => createComment(index)),
+});
 
-// Строим комментарии
-const createComments = () => {
-  return {
-    id: getRandomArrayElement(COMMENTS_ID),
-    avatar: 'img/avatar-' + getRandomArrayElement(COMMENTS_AVATAR) + '.svg',
-    message: getRandomArrayElement(COMMENTS_MESSAGE),
-    name:getRandomArrayElement(COMMENTS_NAMES),
-  };
-};
-
-// Записываем комментарии в массив
-const similarComments = Array.from({length:6}, createComments);
-
-
-// Строим описание фотографии
-const createDescription = () => {
-  return {
-    id: getRandomArrayElement(PHOTO_ID),
-    url: 'photos/{{' + getRandomArrayElement(PHOTO_URL) + '}}.jpg',
-    description: getRandomArrayElement(PHOTO_DESCRIPTION),
-    likes: getRandomArrayElement(PHOTO_LIKES),
-    comments: getRandomArrayElement(similarComments),
-  };
-};
-
-// Записываем описание в массив
-const similarDescription = Array.from({length:25}, createDescription);
-
-
-// !Вспомогательные функции!
-
-
-//Функция, заполняющая массив числами по порядку до максимального
-function fillArray(max) {
-  const array = [];
-  if (max) {for (let i = 1; i <= max;) {
-    array.push(i++);
+//Функцию написал как понимал. Ту, что приводил в примере я не смог понять и применить.
+function createCards(amount){
+  const arr = [];
+  for (let i = 0; i <= amount; i++) {
+    arr.push(createPhotoBlock(i));
   }
-  }
-  return array;
+  return arr;
 }
+
+
+createCards(MAX_CARDS);
 
 //Функция, возращающая случайное целое число (источник: https://myrusakov.ru/js-random-numbers.html):
 function getRandomInRange(min, max) {
