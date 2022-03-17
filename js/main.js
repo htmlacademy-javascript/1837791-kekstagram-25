de1febe783f9f518d0a864474dab527144e6df95
Vitaly <shmal9lk@gmail.com>
2022-03-17 21:08:56 +0500

    Правки

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-const COMMENTS_MESSAGE = ['Всё отлично!',
+const COMMENT_MESSAGES = ['Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-const COMMENTS_NAME = ['Василий', 'Иван', 'Егор', 'Игорь', 'Тарас', 'Марк'];
+const COMMENTS_NAMES = ['Василий', 'Иван', 'Егор', 'Игорь', 'Тарас', 'Марк'];
 
-const PHOTOS_DESCRIPTION = ['Овощные культуры','Ягоды','Стручковые','Бобовые','Грибы'];
+const PHOTOS_DESCRIPTIONS = ['Овощные культуры','Ягоды','Стручковые','Бобовые','Грибы'];
 const PHOTO_MAX_LIKES = 200;
 const PHOTO_MIN_LIKES = 15;
 const AVATARS_TOTAL = 6;
@@ -17,14 +17,14 @@ const EXAMPLE_NUMBER = 10;
 const createComment = (id) => ({
   id,
   avatar: `img/avatar-${getRandomInRange(0, AVATARS_TOTAL)}.svg`,
-  message: COMMENTS_MESSAGE[getRandomInRange(0, COMMENTS_MESSAGE.length - 1)],
-  name: COMMENTS_NAME[getRandomInRange(0, COMMENTS_NAME.length - 1)],
+  message: COMMENT_MESSAGES[getRandomInRange(0, COMMENT_MESSAGES.length - 1)],
+  name: COMMENTS_NAMES[getRandomInRange(0, COMMENTS_NAMES.length - 1)],
 });
 
 const createPhotoBlock = (id) => ({
   id,
   url: `photos/${getRandomInRange(1, 25)}.jpg`,
-  description: PHOTOS_DESCRIPTION[getRandomInRange(0, PHOTOS_DESCRIPTION.length - 1)],
+  description: PHOTOS_DESCRIPTIONS[getRandomInRange(0, PHOTOS_DESCRIPTIONS.length - 1)],
   likes: getRandomInRange(PHOTO_MIN_LIKES, PHOTO_MAX_LIKES),
   comments: Array.from({length: getRandomInRange(1, 2)}, (_, index) => createComment(index)),
 });
@@ -33,7 +33,7 @@ const createPhotoBlock = (id) => ({
 function createCards(amount){
   const arr = [];
   for (let i = 0; i <= amount; i++) {
-    arr.push(createPhotoBlock(i));
+    arr.push( createPhotoBlock(i) );
   }
   return arr;
 }
